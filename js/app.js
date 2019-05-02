2b9c25b05704099364464c73599127875e29ce9f
Ryan Comerford <40415962+ryncmrfrd@users.noreply.github.com>
2019-05-02 20:57:43 +1000

    finished properly

--- js/app.js
+++ js/app.js
@@ -1,11 +1,37 @@
 window.onload = function(){
-	chrome.runtime.onMessage.addListener(function(request, sender) {
-		if (request.action == "getSource") {
-			document.write(request.source)
-		}
-	});
-	
 	chrome.tabs.executeScript(null, {
-    file: "js/inject.js"
-  });
+		file: "js/inject.js"
+	});
+	chrome.runtime.onMessage.addListener(function(request, sender) {
+		
+		if(request.action=="transText") {
+			document.querySelector("#transText").innerHTML = request.source;
+			var xhttp = new XMLHttpRequest();
+				xhttp.onreadystatechange = function() {
+					if (this.readyState == 4 && this.status == 200) {
+
+						var response = JSON.parse(this.responseText);
+
+						document.querySelector("#transResponse").innerHTML = response.text
+
+						document.querySelector("#fromLang").innerHTML = response.lang.split("-")[0]
+						document.querySelector("#toLang").innerHTML = response.lang.split("-")[1]
+
+					}
+				};
+			xhttp.open("GET", "https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20181030T063621Z.0a45b8efb2e70388.6bcd8348dc80843bb85d58bc85e8cd90c112b557&text="+request.source+"&lang=en", true);
+			xhttp.send();
+		}
+		else if(request.action=="url") {
+			if(request.source.indexOf("www.educationperfect.com/app") > 0 && request.source.indexOf("game?mode=0") > 0){
+				document.querySelector("#notsupported").style.display="none";
+				document.querySelector("#supported").style.display="block";
+			}
+		}
+	
+	});
 };
+
+document.querySelector("#btn").onclick = function(){
+  chrome.tabs.create({ url: "https://www.educationperfect.com/app/" });
+}
--- js/inject.js
+++ js/inject.js
@@ -1,34 +1,10 @@
-// @author Rob W <http://stackoverflow.com/users/938089/rob-w>
-// Demo: var serialized_html = DOMtoString(document);
-
-function DOMtoString(document_root) {
-    var html = '',
-        node = document_root.firstChild;
-    while (node) {
-        switch (node.nodeType) {
-        case Node.ELEMENT_NODE:
-            html += node.outerHTML;
-            break;
-        case Node.TEXT_NODE:
-            html += node.nodeValue;
-            break;
-        case Node.CDATA_SECTION_NODE:
-            html += '<![CDATA[' + node.nodeValue + ']]>';
-            break;
-        case Node.COMMENT_NODE:
-            html += '<!--' + node.nodeValue + '-->';
-            break;
-        case Node.DOCUMENT_TYPE_NODE:
-            // (X)HTML documents are identified by public identifiers
-            html += "<!DOCTYPE " + node.name + (node.publicId ? ' PUBLIC "' + node.publicId + '"' : '') + (!node.publicId && node.systemId ? ' SYSTEM' : '') + (node.systemId ? ' "' + node.systemId + '"' : '') + '>\n';
-            break;
-        }
-        node = node.nextSibling;
-    }
-    return html;
-}
-
+if (document.getElementById("question-text").childNodes.length > 1) var transText = document.getElementById("question-text").childNodes[1].innerText;
+else var transText = document.getElementById("question-text").childNodes[0].innerText;
 chrome.runtime.sendMessage({
-    action: "getSource",
-    source: DOMtoString(document)
+    action: "url",
+    source: window.location.href
+});
+chrome.runtime.sendMessage({
+    action: "transText",
+    source: transText
 });
